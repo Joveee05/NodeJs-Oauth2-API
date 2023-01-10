39d1c7d7bdd3d200477cb6f206a5550534a651ec
Brian Etaghene <joveee05@gmail.com>
2023-01-10 18:34:14 +0100

    NODE_ENV

--- utils/email.js
+++ utils/email.js
@@ -1,22 +1,22 @@
 const nodemailer = require('nodemailer');
-
-const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-
-  const mailOptions = {
-    from: 'no-reply@pisqre.com',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
+if (process.env.NODE_ENV === 'development') {
+  const sendEmail = async (options) => {
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
   };
-  await transporter.sendMail(mailOptions);
+}
+const mailOptions = {
+  from: 'no-reply@pisqre.com',
+  to: options.email,
+  subject: options.subject,
+  text: options.message,
 };
+await transporter.sendMail(mailOptions);
 
 module.exports = sendEmail;
