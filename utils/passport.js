c98af44c754362ee9b3780cf84a0e5d093f38a81
Brian Etaghene <joveee05@gmail.com>
2023-03-04 16:00:07 +0100

    [Update]

--- utils/passport.js
+++ utils/passport.js
@@ -1,5 +1,6 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
+
 const User = require('../models/userModel');
 const crypto = require('crypto');
 const dotenv = require('dotenv');
