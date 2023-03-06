72667613f2d5f6bf5ef67665b6280e0116074f84
Brian Etaghene <joveee05@gmail.com>
2023-03-06 15:59:57 +0100

    [Updated] Failure and success URLs for Oauth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,25 @@ const passport = require('passport');
 const { nocache, generateAccessToken } = require('../controllers/agora');
 const authController = require('../controllers/authController');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+const getAccessToken = (user, res) => {
+  const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+  return token;
+};
 
 router.get(
   '/google',
@@ -11,9 +30,15 @@ router.get(
 
 router.get(
   '/google/callback',
-  passport.authenticate('google', { failureRedirect: '/api/users/login' }),
+  passport.authenticate('google', {
+    failureRedirect: process.env.FAILURE_REDIRECT_URL,
+  }),
   (req, res) => {
-    res.redirect('/api/users/me');
+    const token = getAccessToken(req.user._id, res);
+    res.writeHead(302, {
+      Location: process.env.SUCCESS_REDIRECT_URL + token,
+    });
+    res.end();
   }
 );
 
