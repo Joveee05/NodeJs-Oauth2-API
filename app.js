4374ee7676d920a4ab6d950653596f60aa585fca
Brian Etaghene <joveee05@gmail.com>
2023-03-10 18:23:24 +0100

    Removed Rate Limit

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
