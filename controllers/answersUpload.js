dc28c9ac598e26bb8d5add69d7f8e991889c462b
Brian Etaghene <joveee05@gmail.com>
2023-03-18 01:58:25 +0100

    [Added] Answers upload controller

---
+++ controllers/answersUpload.js
@@ -0,0 +1,37 @@
+const multer = require('multer');
+const sharp = require('sharp');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Oops... Sorry, Please upload images only', 400), false);
+  }
+};
+
+const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
+
+exports.uploadAnswerFile = upload.single('answer');
+
+exports.saveToFolder = catchAsync(async (req, res, next) => {
+  if (!req.file) {
+    return next(new AppError('Please upload a file', 400));
+  }
+  const questionId = req.params.id;
+  req.file.filename = `answer-${questionId}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .toFormat('jpeg')
+    .jpeg({ quality: 100 })
+    .toFile(`public/answers/${req.file.filename}`);
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Upload successful',
+    data: req.file.filename,
+  });
+});
