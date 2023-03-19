4c063f7022bf79823b2f248361b563d44865be7c
Brian Etaghene <joveee05@gmail.com>
2023-03-19 14:57:56 +0100

    [Added] myQuestion, myAnswer and search for tutors

--- controllers/tutor/tutorController.js
+++ controllers/tutor/tutorController.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const Tutor = require('../../models/tutorModel');
+const Question = require('../../models/questions');
+const Answer = require('../../models/answer');
 const multer = require('multer');
 const sharp = require('sharp');
 const AppError = require('../../utils/appError');
 const Email = require('../../utils/email');
 const catchAsync = require('../../utils/catchAsync');
+const APIFeatures = require('../../utils/apiFeatures');
 
 const multerStorage = multer.memoryStorage();
 
@@ -178,5 +181,119 @@ exports.verifyTutor = catchAsync(async (req, res, next) => {
     });
   } else {
     return next(new AppError('Tutor verification failed'));
+  }
+});
+
+exports.askQuestion = catchAsync(async (req, res, next) => {
+  let keywords = [];
+  if (req.body.keywords) keywords = req.body.keywords;
+
+  const body = {
+    questionBody: req.body.questionBody,
+    title: req.body.title,
+    iP: req.ip,
+    branch: req.body.branch,
+    tutor: req.user.id,
+    keywords,
+  };
+  const question = await Question.create(body);
+
+  res.status(201).json({
+    status: 'success',
+    message: 'Question created successfully',
+    data: question,
+  });
+});
+
+exports.tutorAnswer = catchAsync(async (req, res, next) => {
+  const questionId = req.params.id;
+  let body = {
+    answeredByTutor: req.user.id,
+    question: questionId,
+    answer: req.body.answer,
+    answerTimeStamp: new Date(),
+    answerModifiedTimeStamp: new Date(),
+  };
+
+  const answer = await Answer.create(body).then(
+    await Question.findByIdAndUpdate(questionId, {
+      $push: {
+        answeredBy: {
+          user: req.user.fullName,
+          role: req.user.role,
+          image: req.user.image,
+          answer: req.body.answer,
+        },
+      },
+      $inc: { answers: 1 },
+    })
+  );
+  res.status(201).json({
+    status: 'success',
+    message: 'Answer created successfully',
+    data: answer,
+  });
+});
+
+exports.myQuestions = catchAsync(async (req, res, next) => {
+  const allMyQuestions = await Question.find({ tutor: req.user.id });
+  const features = new APIFeatures(
+    Question.find({ tutor: req.user.id }),
+    req.query
+  )
+    .sort()
+    .paginate();
+  const getMyQuestions = await features.query;
+
+  if (getMyQuestions.length < 1) {
+    return next(new AppError('Oops... No questions found!!', 404));
+  }
+  res.status(200).json({
+    status: 'success',
+    allMyQuestions: allMyQuestions.length,
+    results: getMyQuestions.length,
+    data: getMyQuestions,
+  });
+});
+
+exports.myAnswers = catchAsync(async (req, res, next) => {
+  const allMyAnswers = await Answer.find({ answeredByTutor: req.user.id });
+  const features = new APIFeatures(
+    Answer.find({ answeredByTutor: req.user.id }),
+    req.query
+  )
+    .sortByTimeStamp()
+    .paginate();
+  const getMyAnswers = await features.query;
+
+  if (getMyAnswers.length < 1) {
+    return next(new AppError('Oops... No answers found!!', 404));
+  }
+  res.status(200).json({
+    status: 'success',
+    allMyAnswers: allMyAnswers.length,
+    results: getMyAnswers.length,
+    data: getMyAnswers,
+  });
+});
+
+exports.searchTutor = catchAsync(async (req, res, next) => {
+  const data = await Tutor.find({
+    $text: { $search: req.query.course, $caseSensitive: false },
+  });
+
+  if (data.length < 1) {
+    return next(
+      new AppError(
+        'Oops... No tutor found. This may be due to a spelling error. Try searching again.',
+        404
+      )
+    );
+  } else {
+    res.status(200).json({
+      status: 'success',
+      results: data.length,
+      data,
+    });
   }
 });
