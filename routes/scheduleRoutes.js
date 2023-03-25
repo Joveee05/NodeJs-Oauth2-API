4bd1e0def1230f45693d3330e31f63d1f5d6aa4d
Brian Etaghene <joveee05@gmail.com>
2023-03-26 00:20:39 +0100

    [Update]

--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -5,15 +5,22 @@ const scheduleController = require('../controllers/scheduleController');
 const auth = require('../controllers/tutor/auth');
 const router = express.Router({ mergeParams: true });
 
+router.get('/schedule-between-dates/:tutorId', scheduleController.dateQuery);
+
 router.use(auth.protect);
 
 router.post('/create_my_schedule', scheduleController.createSchedule);
 
+router.post(
+  '/create_multiple_schedules',
+  scheduleController.createMultipleSchedule
+);
+
 router.get('/get-weekly-schedule', scheduleController.getWeeklyPlan);
 
-router.get('/schedule-between-date', scheduleController.dateQuery);
+router.get('/mySchedule', scheduleController.getMySchedule);
 
-router.get('/mySchedule', scheduleController.getSchedule);
+router.get('/get_schedule/:id', scheduleController.getSchedule);
 
 router.patch('/change_schedule/:id', scheduleController.updateSchedule);
 
@@ -49,6 +56,44 @@ router.delete('/delete_schedule/:id', scheduleController.deleteSchedule);
 
 /**
  * @swagger
+ * /schedules/get_schedule/{id}:
+ *      get:
+ *        summary: Get schedule by id
+ *        tags: [Schedules]
+ *        parameters:
+ *          - in: path
+ *            name: id
+ *            required: true
+ *            description: The schedule id
+ *        responses:
+ *          200:
+ *            description: success
+ *            content:
+ *               application/json:
+ *                      schema:
+ *                         example:
+ *                            status: success
+ *                            message: Schedule found
+ *                            data: [{
+ *                               _id: 641854cac888be8f1b2e42a6,
+ *                               tutorId: 64179c2dc888be8f1b2e4072,
+ *                               startDate: [
+ *                                   2023-03-20T06:00:00.000Z,
+ *                                      ],
+ *                               endDate: [
+ *                                   2023-03-21T10:00:00.000Z,
+ *                                      ],
+ *                               booked: false,
+ *                                 }]
+ *          401:
+ *            description: You are not logged in. Please log in to get access
+ *          404:
+ *            description: Oops.. No schedule found. Please check that the id is correct
+ *
+ */
+
+/**
+ * @swagger
  * /schedules/change_schedule/{id}:
  *    patch:
  *      summary: Edit or update tutor schedule
@@ -73,6 +118,8 @@ router.delete('/delete_schedule/:id', scheduleController.deleteSchedule);
  *            application/json:
  *              schema:
  *                $ref: '#/components/schemas/Booking'
+ *        401:
+ *            description: You are not logged in. Please log in to get access
  *        404:
  *          description: No schedule found. Please check that the Id is correct
  *        500:
@@ -93,7 +140,7 @@ router.delete('/delete_schedule/:id', scheduleController.deleteSchedule);
  *                  schema:
  *                      $ref: '#/components/schemas/Schedule'
  *          401:
- *            description: You're not logged in. Please log in to get access
+ *            description: You are not logged in. Please log in to get access
  *          404:
  *            description: No schedule found for this tutor
  *
@@ -115,6 +162,8 @@ router.delete('/delete_schedule/:id', scheduleController.deleteSchedule);
  *      responses:
  *          200:
  *            description: Schedule Deleted
+ *          401:
+ *            description: You are not logged in. Please log in to get access
  *          404:
  *            description: No schedule found. Please check that the Id is correct
  */
@@ -143,7 +192,8 @@ router.delete('/delete_schedule/:id', scheduleController.deleteSchedule);
  *                       startDate: 2023-03-15
  *                       endDate: 2023-03-15
  *                       booked: false
- *
+ *          401:
+ *            description: You are not logged in. Please log in to get access
  *          500:
  *            description: Internal server error.
  */
@@ -176,7 +226,7 @@ router.delete('/delete_schedule/:id', scheduleController.deleteSchedule);
  *                               week: 11,
  *                                 }]
  *          401:
- *            description: You're not logged in. Please log in to get access
+ *            description: You are not logged in. Please log in to get access
  *          404:
  *            description: Oops.. No schedule found for this year
  *
@@ -184,11 +234,15 @@ router.delete('/delete_schedule/:id', scheduleController.deleteSchedule);
 
 /**
  * @swagger
- * /schedules/schedule-between-date:
+ * /schedules/schedule-between-dates:
  *     get:
  *       summary: Get schedule between two dates
  *       tags: [Schedules]
  *       parameters:
+ *          - in: path
+ *            name: tutor id
+ *            required: true
+ *            description: The id of the tutor
  *          - in: query
  *            name: from
  *            required: true
@@ -217,10 +271,66 @@ router.delete('/delete_schedule/:id', scheduleController.deleteSchedule);
  *                               booked: false,
  *                                 }]
  *          401:
- *            description: You're not logged in. Please log in to get access
+ *            description: You are not logged in. Please log in to get access
  *          404:
  *            description: Oops.. No schedule found between these dates
  *
+ */
+
+/**
+ * @swagger
+ * /schedules/create_multiple_schedules:
+ *    post:
+ *      summary: Create many schedules at once
+ *      tags: [Schedules]
+ *      parameters:
+ *        - in: query
+ *          name: numOfSchedule
+ *          required: true
+ *          description: The number of schedules you want to create at a time
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *              schema:
+ *                 $ref: '#/components/schemas/Schedule'
+ *      responses:
+ *          201:
+ *            description: Schedule created successfully
+ *            content:
+ *                application/json:
+ *                    schema:
+ *                       $ref: '#/components/schemas/Schedule'
+ *                    example:
+ *                       status: success
+ *                       message: Schedule created successfully
+ *                       results: 2
+ *                       data: [
+ *                        {
+ *                         tutorId: 641712f498cb1ebd4f5442d8,
+ *                         startDate: [
+ *                                2023-03-15
+ *                                    ],
+ *                         endDate: [
+ *                              2023-03-15
+ *                                  ],
+ *                         booked: false,
+ *                          },
+ *                        {
+ *                         tutorId: 641712f498cb1ebd4f5442d8,
+ *                         startDate: [
+ *                                2023-03-15
+ *                                    ],
+ *                         endDate: [
+ *                              2023-03-15
+ *                                  ],
+ *                         booked: false
+ *                          },
+ *                            ]
+ *          401:
+ *            description: You are not logged in. Please log in to get access
+ *          500:
+ *            description: Internal server error.
  */
 
 module.exports = router;
