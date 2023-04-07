3c2521565216076d0f5f5fa31a50380d3c55e4a3
Brian Etaghene <joveee05@gmail.com>
2023-04-07 02:01:38 +0100

    [Added] createNotification and updateNotification functions

--- controllers/utility.js
+++ controllers/utility.js
@@ -1,5 +1,7 @@
 //For resolving circular dependencies
 const Vote = require('../models/vote');
+const Notification = require('../models/notificationModel');
+const AppError = require('../utils/appError');
 
 async function removeVotesForObjectId(id) {
   try {
@@ -17,6 +19,33 @@ async function removeVotesForObjectId(id) {
   }
 }
 
+async function createNotification(message, userID, questionID, answerID) {
+  try {
+    const payload = {
+      message,
+      userID,
+      questionID,
+      answerID,
+    };
+    const notification = await Notification.create(payload);
+  } catch (err) {
+    return new AppError('Failed to create notification', 400);
+  }
+}
+
+const updateNotification = async function (id) {
+  const result = await Notification.findByIdAndUpdate(id, { new: true });
+  if (result) {
+    result.read = true;
+    const notification = await result.save({ validateBeforeSave: false });
+    return notification;
+  } else {
+    return new AppError('Inavlid notification id', 400);
+  }
+};
+
 module.exports = {
   removeVotesForObjectId,
+  createNotification,
+  updateNotification,
 };
