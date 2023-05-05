fbc314df08fc6b710478bce65e42c1c41565f76d
Brian Etaghene <joveee05@gmail.com>
2023-05-05 13:49:59 +0100

    [Modified]

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(
-    `Pisqre App running in ${process.env.NODE_ENV} mode on port ${port}...`
+    `Pisqre API running in ${process.env.NODE_ENV} mode on port ${port}...`
   );
 });
 
