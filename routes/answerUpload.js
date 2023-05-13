2f17a7c03502901aa7698609e5454b17ab51acad
Brian Etaghene <joveee05@gmail.com>
2023-05-13 13:00:53 +0100

    [Modified]

--- routes/answerUpload.js
+++ routes/answerUpload.js
@@ -6,7 +6,6 @@ const User = require('../models/userModel');
 const { GridFsStorage } = require('multer-gridfs-storage');
 const fs = require('fs');
 const router = express.Router();
-let { createNotification } = require('../controllers/utility');
 const dotenv = require('dotenv');
 const AppError = require('../utils/appError');
 
@@ -76,44 +75,28 @@ const upload = multer({ storage }).single('answer');
  *         description: Document Uploaded
  */
 
-router.post(
-  '/upload_answers/:id/questions/:questionId',
-  async (req, res, next) => {
-    const answerId = req.params.id;
-    const questionId = req.params.questionId;
-    const question = await Question.findById(questionId);
-    if (!question) {
-      return next(new AppError('Invalid or no question id', 400));
-    }
-    const userID = question.user._id;
-    const message = 'You have an answer to one of your questions';
-
-    await Question.findByIdAndUpdate(questionId, {
-      $inc: { answers: 1 },
-    });
-
-    const response = upload(req, res, (err) => {
-      if (!req.file) {
-        return res
-          .status(400)
-          .send({ error: 'No file attached. Please attach a file' });
-      } else {
-        createNotification(
-          'answer to question',
-          message,
-          userID,
-          questionId,
-          answerId
-        );
-        return res.status(200).json({
-          status: 'success',
-          message: 'Document uploaded successfully',
-          data: req.file,
-        });
-      }
-    });
+router.post('/upload_answers/:id/questions/:questionId', async (req, res, next) => {
+  const answerId = req.params.id;
+  const questionId = req.params.questionId;
+  const question = await Question.findById(questionId);
+  if (!question) {
+    return next(new AppError('Invalid or no question id', 400));
   }
-);
+  const userID = question.user._id;
+  const message = 'You have an answer to one of your questions';
+
+  const response = upload(req, res, (err) => {
+    if (!req.file) {
+      return res.status(400).send({ error: 'No file attached. Please attach a file' });
+    } else {
+      return res.status(200).json({
+        status: 'success',
+        message: 'Document uploaded successfully',
+        data: req.file,
+      });
+    }
+  });
+});
 
 /**
  * @swagger
@@ -203,21 +186,19 @@ router.get('/download_answer/:id', async (req, res) => {
  */
 
 router.delete('/:id', async (req, res) => {
-  bucket
-    .find({ _id: new mongoose.Types.ObjectId(req.params.id) })
-    .toArray((err, file) => {
-      if (!file || file.length == 0) {
-        res.status(200).json({
-          status: 'failed',
-          message: 'No document exist',
-        });
-      }
-      bucket.delete(file[0]._id);
+  bucket.find({ _id: new mongoose.Types.ObjectId(req.params.id) }).toArray((err, file) => {
+    if (!file || file.length == 0) {
       res.status(200).json({
-        status: 'success',
-        message: 'Document deleted',
+        status: 'failed',
+        message: 'No document exist',
       });
+    }
+    bucket.delete(file[0]._id);
+    res.status(200).json({
+      status: 'success',
+      message: 'Document deleted',
     });
+  });
 });
 
 module.exports = router;
