85aee50f3d7cf8f81048965697dce96e8e3d9cce
Brian Etaghene <joveee05@gmail.com>
2023-05-30 22:16:57 +0100

    [Added] notification and pisqreId

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -7,7 +7,10 @@ const AppError = require('../utils/appError');
 const APIFeatures = require('../utils/apiFeatures');
 const bookingEmail = require('../utils/bookingEmail');
 const catchAsync = require('../utils/catchAsync');
-const { updateNumOfBookings, updateSchedule } = require('./utility');
+const { createNotification, updateNumOfBookings, updateSchedule } = require('./utility');
+
+const message1 = 'You have successfully booked a Live Session - Admin';
+const message2 = 'This is to notify you that you have an upcoming Live Session - Admin';
 
 exports.bookSession = catchAsync(async (req, res, next) => {
   const body = {
@@ -19,6 +22,7 @@ exports.bookSession = catchAsync(async (req, res, next) => {
     sessionType: req.body.sessionType,
     bookedBy: req.user.id,
     time: req.body.time,
+    pisqreId: Math.floor(Math.random() * 100000000 + 1),
   };
   const user = await User.findById(body.bookedBy);
   const tutor = await Tutor.findById(body.tutor);
@@ -26,8 +30,12 @@ exports.bookSession = catchAsync(async (req, res, next) => {
   if (booking) {
     await new bookingEmail(user, tutor, booking).confirmBooking();
     await new bookingEmail(tutor, user, booking).notifyTutor();
-    updateSchedule(req.query.schedule);
-    updateNumOfBookings(body.tutor);
+    await createNotification('live session', message1, body.bookedBy, booking.id);
+    await createNotification('live session', message2, body.tutor, booking.id);
+    await updateSchedule(req.query.schedule);
+    await updateNumOfBookings(body.tutor);
+  } else {
+    return next(new AppError('There was an issue creating a booking', 500));
   }
   res.status(201).json({
     status: 'success',
@@ -39,9 +47,7 @@ exports.bookSession = catchAsync(async (req, res, next) => {
 exports.getAllBookings = catchAsync(async (req, res, next) => {
   const booking = await Booking.find().populate('bookedBy').sort('-createdAt');
   if (booking.length < 1) {
-    return next(
-      new AppError('No Live Session Booking found in the database.', 404)
-    );
+    return next(new AppError('No Live Session Booking found in the database.', 404));
   }
   res.status(200).json({
     status: 'success',
@@ -54,12 +60,7 @@ exports.getAllBookings = catchAsync(async (req, res, next) => {
 exports.getBooking = catchAsync(async (req, res, next) => {
   const booking = await Booking.findById(req.params.id).populate('bookedBy');
   if (!booking) {
-    return next(
-      new AppError(
-        'No Live Session Booking found in the database with that ID.',
-        404
-      )
-    );
+    return next(new AppError('No Live Session Booking found in the database with this ID.', 404));
   } else {
     res.status(200).json({
       status: 'success',
@@ -69,12 +70,8 @@ exports.getBooking = catchAsync(async (req, res, next) => {
 });
 
 exports.getMyBookings = catchAsync(async (req, res, next) => {
-  const features = new APIFeatures(
-    Booking.find({ tutor: req.user.id }),
-    req.query
-  )
-    .sort()
-    .paginate();
+  const allMyBookings = await Booking.find({ tutor: req.user.id });
+  const features = new APIFeatures(Booking.find({ tutor: req.user.id }), req.query).sort().paginate();
   const myBookings = await features.query;
   if (myBookings.length < 1) {
     return next(new AppError('Oops... No bookings found!!', 404));
@@ -82,23 +79,23 @@ exports.getMyBookings = catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: 'success',
     message: 'Bookings found',
+    allMyBookings: allMyBookings.length,
     results: myBookings.length,
     data: myBookings,
   });
 });
 
 exports.getUserBookings = catchAsync(async (req, res, next) => {
-  const features = new APIFeatures(
-    Booking.find({ bookedBy: req.user.id }),
-    req.query
-  ).paginate();
+  const allMyBookings = await Booking.find({ bookedBy: req.user.id });
+  const features = new APIFeatures(Booking.find({ bookedBy: req.user.id }), req.query).sort().paginate();
   const myBookings = await features.query;
-  if (myBookings.length < 1) {
+  if (myBookings.length < 1 || allMyBookings.length < 1) {
     return next(new AppError('Oops... No bookings found!!', 404));
   }
   res.status(200).json({
     status: 'success',
     message: 'Bookings found',
+    allMyBookings: allMyBookings.length,
     results: myBookings.length,
     data: myBookings,
   });
@@ -109,12 +106,7 @@ exports.updateBooking = catchAsync(async (req, res, next) => {
     new: true,
   });
   if (!booking) {
-    return next(
-      new AppError(
-        'No Live Session Booking found in the database with that ID.',
-        404
-      )
-    );
+    return next(new AppError('No Live Session Booking found in the database with that ID.', 404));
   } else {
     res.status(200).json({
       status: 'success',
@@ -127,12 +119,7 @@ exports.updateBooking = catchAsync(async (req, res, next) => {
 exports.deleteBooking = catchAsync(async (req, res, next) => {
   const booking = await Booking.findByIdAndDelete(req.params.id);
   if (!booking) {
-    return next(
-      new AppError(
-        'No Live Session Booking found in the database with that ID.',
-        404
-      )
-    );
+    return next(new AppError('No Live Session Booking found in the database with that ID.', 404));
   } else {
     res.status(200).json({
       status: 'success',
